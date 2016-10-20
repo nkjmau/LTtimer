f2a829f10a07de10a6a7766a9651c4925ee45b2a
nkjmau <nkjmau@yahoo.co.jp>
2016-10-21 00:24:32 +0900

    とりあえずcountUpで実装した

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 window.addEventListener('load', function(){
+  var timeCount = 0;
+  var intervalID;
   var state = 0;
   var startBtn = document.getElementById('start-btn');
   var stopBtn = document.getElementById('stop-btn');
+  var timerZone = document.getElementById('timer');
+
+  function countUp(){
+    timeCount++;
+    timerZone.innerHTML = ('00' + Math.floor(timeCount/60)).slice(-2) + ' : ' + ('00' + timeCount%60).slice(-2);
+  }
 
   function startFunc(){
     if(state === 0){
@@ -10,6 +18,8 @@ window.addEventListener('load', function(){
       startBtn.style.color = '#daa520';
       stopBtn.style.backgroundColor = '#8b4513';
       stopBtn.style.color = 'white';
+
+      intervalID = setInterval(countUp, 1000);
     }
   }
   function stopFunc(){
@@ -20,6 +30,8 @@ window.addEventListener('load', function(){
       startBtn.style.color = 'white';
       stopBtn.style.backgroundColor = '#daa520';
       stopBtn.style.color = '#daa520';
+
+      clearInterval(intervalID);
     }
   }
 
